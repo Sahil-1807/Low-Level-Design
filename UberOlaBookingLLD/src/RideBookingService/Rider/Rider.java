7e0b0315ba1d7821b8f930945130a5fc671562e6
sahil <sahilsingh180799@gmail.com>
2024-12-16 00:46:24 +0530

    adding first commit for lld of uber ola

---
+++ UberOlaBookingLLD/src/RideBookingService/Rider/Rider.java
@@ -0,0 +1,27 @@
+package RideBookingService.Rider;
+
+public class Rider {
+    private String name;
+    private String rating;
+
+    public Rider(String name, String rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
