5976182024b43b4e80f840c7724a7a74a88055cd
sahil <sahilsingh180799@gmail.com>
2025-03-01 01:08:26 +0530

    updated theatre controller with some other methods

--- MovieTicketBookingLLD/src/main/java/com/example/moviebooking/api/TheatreController.java
+++ MovieTicketBookingLLD/src/main/java/com/example/moviebooking/api/TheatreController.java
@@ -1,5 +1,7 @@
 package com.example.moviebooking.api;
 
+import com.example.moviebooking.models.Screen;
+import com.example.moviebooking.models.Theatre;
 import com.example.moviebooking.services.TheatreService;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
@@ -10,5 +12,15 @@ public class TheatreController {
 
     public String createTheatre(@NonNull final String theatreName){
         return theatreService.createTheatre(theatreName).getId();
+    }
+
+    public String createScreenInTheatre(@NonNull final String screeName, @NonNull final String theatreId){
+        Theatre theatre = theatreService.getTheatre(theatreId);
+        return theatreService.createScreenInTheatre(screeName, theatre).getId();
+    }
+
+    public String createSeatInScreen(@NonNull final Integer rowNo, @NonNull final Integer seatNo, @NonNull final String screenId){
+        final Screen screen = theatreService.getScreen(screenId);
+        return theatreService.createSeatInScreen(rowNo, seatNo, screen).getId();
     }
 }
